741dd86bcecfca1c4d4ee555f6b7bdc88e05064c
cieldinoegg <cieldinoegg@naver.com>
2022-02-09 22:46:36 +0900

    블로그 띄우기

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '블로그 초대',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue
+      ),
+      home: WebViewExample(),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> {
+  WebViewController? _webViewController;
+
+  @override
+  void initState() {
+    super.initState();
+    // Enable hybrid composition.
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () => _goBack(context),
+      child: Scaffold(
+        body: WebView(
+            initialUrl: 'https://blog.naver.com/cieldinoegg',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (_webViewController == null) {
+      return true;
+    }
+    if (await _webViewController!.canGoBack()) {
+      _webViewController!.goBack();
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+}
